265b6ec72a657839b26816ce55c4910a659c3b2a
Valentin-Paramonov <valentin.paramonov@ctco.lv>
2013-09-23 19:06:13 +0300

    Started implementing HCBC partitioning procedure.

--- src/main/java/paramonov/valentin/fiction/hcbc/HCBCModule.java
+++ src/main/java/paramonov/valentin/fiction/hcbc/HCBCModule.java
@@ -3,21 +3,30 @@ package paramonov.valentin.fiction.hcbc;
 import paramonov.valentin.fiction.collections.Pair;
 import paramonov.valentin.fiction.image.Image;
 
-public class HCBCModule {
-    public Pair<HCBCTree, Image> hcbcEncode(Image img) {
-        HCBCTree tree = qtPartition(img);
+import java.util.concurrent.ForkJoinPool;
 
-        return new Pair<>(tree, img);
+public class HCBCModule {
+    public Pair<HCBCTree, Image> hcbcEncode(
+        Image img, double tolerance, int maxLevel) {
+
+        Image imgCopy =
+            new Image(
+                img.getARGB(), img.getWidth(), img.getHeight());
+
+        HCBCTree tree = qtPartition(imgCopy, tolerance, maxLevel);
+
+        return new Pair<>(tree, imgCopy);
     }
 
-    public double[][] calculateTC(Image img) {
+    public double[][][] calculateTC(Image img) {
         return calculateTC(img, false);
     }
 
-    public double[][] calculateTC(Image img, boolean produceGrayImage) {
+    public double[][][] calculateTC(Image img, boolean produceGrayImage) {
         int[] colors = img.getARGB();
-
-        double[][] tc = new double[colors.length][3];
+        int w = img.getWidth();
+        int h = img.getHeight();
+        double[][][] tc = new double[w][h][3];
 
         for(int i = 0; i < colors.length; i++) {
             int r = (colors[i] >> 16) & 0xff;
@@ -27,13 +36,16 @@ public class HCBCModule {
 
             if(sum == 0) continue;
 
-            tc[i][0] = r / sum;
-            tc[i][1] = g / sum;
-            tc[i][2] = b / sum;
+            int x = i % w;
+            int y = i / w;
+
+            tc[x][y][0] = r / sum;
+            tc[x][y][1] = g / sum;
+            tc[x][y][2] = b / sum;
 
             if(produceGrayImage) {
                 int weightedSum =
-                    (int) (r * tc[i][0] + g * tc[i][1] + b * tc[i][2]);
+                    (int) (r * tc[x][y][0] + g * tc[x][y][1] + b * tc[x][y][2]);
 
                 colors[i] =
                     (colors[i] & 0xff000000) |
@@ -45,8 +57,20 @@ public class HCBCModule {
     }
 
 
-    public HCBCTree qtPartition(Image img) {
-        double[][] tc = calculateTC(img, true);
-        return null;
+    public HCBCTree qtPartition(
+        Image img, double tolerance, int maxLevel) {
+
+        int w = img.getWidth();
+        int h = img.getHeight();
+        double[][][] tc = calculateTC(img, true);
+        HCBCTree tree = new HCBCTree();
+        ForkJoinPool pool = new ForkJoinPool();
+        HCBCPartition partitionTask =
+            new HCBCPartition(
+                tree, tc, tolerance, maxLevel, 0, 0, w, h);
+
+        pool.invoke(partitionTask);
+
+        return tree;
     }
 }
---
+++ src/test/java/paramonov/valentin/fiction/hcbc/HCBCPartitionTest.java
@@ -0,0 +1,39 @@
+package paramonov.valentin.fiction.hcbc;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HCBCPartitionTest {
+    private HCBCPartition partition;
+    private double[][][] tc = {
+        {{0, 0, 0}, {1., 2., 3.}},
+        {{4., 5., 6.}, {7., 8., 9.}}};
+
+    @Test
+    public void testCalculateMeanTC_ProducesExpectedValues_WholeBlockStartAtZero() {
+        partition =
+            new HCBCPartition(
+                null, tc, 0, 0, 0, 0, 2, 2);
+
+        double[] meanValues = partition.calculateMeanTC();
+
+        double meanTCB = meanValues[1];
+
+        assertThat(meanTCB, equalTo(3.75));
+    }
+
+    @Test
+    public void testCalculateMeanTC_ProducesExpectedValues_SubBlockShifted() {
+        partition =
+            new HCBCPartition(
+                null, tc, 0, 0, 1, 1, 1, 1);
+
+        double[] meanValues = partition.calculateMeanTC();
+
+        double meanTCB = meanValues[1];
+
+        assertThat(meanTCB, equalTo(8.));
+    }
+}
